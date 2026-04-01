07a69ac1cfe08368604fb67b48a05c45e147c2bf
amber-moon <jademoon333@hotmail.com>
2026-04-01 22:21:21 +0100

    Update video URL

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
     {
       id: 'fireplace',
       name: 'Fireplace',
-      videoUrl: 'https://res.cloudinary.com/dekamm1rd/video/upload/v1775071900/FireplaceTest_r92s3l.mov',
+      videoUrl: 'https://res.cloudinary.com/dekamm1rd/video/upload/v1775071900/FireplaceTest_r92s3l.mov?dl=1',
       ambientSoundsUrl: 'https://placeholder-audio.example.com/fireplace-crackle.mp3',
       meditationMusicUrl: 'https://placeholder-audio.example.com/meditation-music.mp3',
     },
