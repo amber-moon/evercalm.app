66a377e31fa6f69475f840c8e50a807eb057231c
amber-moon <jademoon333@hotmail.com>
2026-04-04 18:35:54 +0100

    Enable scroll

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,6 @@ export default function App({ Component, pageProps }) {
           }
           html, body, #__next {
             width: 100%;
-            height: 100%;
-            overflow: hidden;
           }
         `}</style>
       </Head>
