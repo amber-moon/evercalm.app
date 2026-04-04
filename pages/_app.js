3b148af532de2b35eb435cc3dc431696f84518ae
amber-moon <jademoon333@hotmail.com>
2026-04-04 17:13:38 +0100

    Update web layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,19 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <style>{`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          html, body, #__next {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+          }
+        `}</style>
       </Head>
       <Component {...pageProps} />
     </>
